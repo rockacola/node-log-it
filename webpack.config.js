ebb6b55bd19797afacd2780b5588c9c8cead7073
Travis <travis@localhost.dev>
2018-08-18 19:43:43 +1000

    Rename folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.ts',
+  entry: './lib/index.ts',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
